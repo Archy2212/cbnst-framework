fc170241d6f06ccae245684ccf5ba4fa22e3e422
Archy Mathur <archy2212@gmail.com>
2018-08-08 16:21:19 +0530

    first-method

---
+++ node1.js
@@ -0,0 +1,32 @@
+
+var f = function(x){
+return ((x*x*x)-(5*x)+1);
+}
+
+var main=function(a,b){
+  if(f(a)*f(b)>=0)
+  {
+    console.log("the interval is wrong!");
+  }
+  else {
+    console.log("right interval");
+  }
+  var x=0;
+  var x1=0;
+  var i=0;
+  do{
+    var x=(a+b)/2;
+    if(x==x1){
+      console.log("the root is:"+x);
+      break;
+    }
+    x1=x;
+    i++;
+    if(f(a)*f(x)<0)
+    b=x;
+    else {
+      a=x;
+    }
+  }while (f(a)*f(b)<0)
+}
+main(0,1);
